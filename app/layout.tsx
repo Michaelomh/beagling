6627623024cc518527b307354e28e02d22aaa106
Michael Ong <michael.ong@constructor.io>
2023-07-16 14:57:18 +0800

    implement nextjs + shadcn-ui

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Beagling',
+  description: 'Track your habits with Beagling!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
