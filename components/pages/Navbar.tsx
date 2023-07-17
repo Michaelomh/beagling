d1d74ff832727557b1de0e9c42cbbd2a4db23380
Michael Ong <michael.ong@constructor.io>
2023-07-17 13:16:43 +0800

    implement menu page

--- components/pages/Navbar.tsx
+++ components/pages/Navbar.tsx
@@ -1,25 +1,30 @@
 "use client"
 
-import { LayoutList, Menu, TrendingUp } from "lucide-react"
+import { LayoutList, Menu as MenuIcon, TrendingUp } from "lucide-react"
 import Link from "next/link"
-import {usePathname} from "next/navigation"
+import { Menu } from "./menu/Menu"
+import { useState } from "react"
 
 export const Navbar = () => {
-  const pathName = usePathname()
-  console.log(pathName)
+  const [isMenuShowing, setMenuShowing] = useState(false)
   
   return (
     <div className="h-12 flex justify-between items-center px-4 bg-blue-300">
-      <Link href={"/settings"}>
-        <Menu />
-      </Link>
-      <span>Push up Tracker</span>
+      <MenuIcon onClick={() => {
+        console.log('menu icon clicked')
+        setMenuShowing(true)
+      }}/>
+      <span>Push up Tracker{isMenuShowing.toString()}</span>
       <div className="flex flex-row gap-2">
         <Link href={"/records"}>
           <LayoutList />
         </Link>
         {/* <TrendingUp /> */}
       </div>
+      {
+        isMenuShowing &&
+        <Menu hideMenu={setMenuShowing}/>
+      }
     </div>
   )
 }
