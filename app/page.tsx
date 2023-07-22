d2e44cf1c0562fccc3f6e4d22a2d1e786029cd55
Michael Ong <michael.ong@constructor.io>
2023-07-23 02:06:04 +0800

    implment counter + monthly stats

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { SubmitButton } from "./SubmitButton";
+'use client'
+
+import { Counter } from "@/components/pages/project/Counter";
+import MonthlyStatsBox from "@/components/pages/project/MonthlyStatsBox";
+import StatsBox from "@/components/pages/project/StatsBox";
 
 export default function Home() {
-
   return (
-    <div className="p-4 text-center">
-      <div className="flex flex-col items-center">
-        <span className="text-lg italic slate text-slate-500">This Year</span>
-        <h1 className="font-bold text-6xl">3720</h1>
-      </div>
-      <div className="flex flex-col items-center">
-        <span className="text-lg italic slate text-slate-500">This month (July)</span>
-        <h1  className="font-bold text-6xl">410 / 560</h1>
-      </div>
-      <div className="mt-4">
-        <div className="flex flex-row gap-2 justify-between mb-4">
-          <div className="flex flex-row gap-4">
-            <Button>-1</Button>
-            <Button>-5</Button>
-          </div>
-          <h1 className="font-bold text-4xl">20</h1>
-          <div className="flex flex-row gap-4">
-            <Button>+5</Button>
-            <Button>+1</Button>
-          </div>
-        </div>
-        <SubmitButton />
-      </div>
+    <div className="p-4 grid gap-4 grid-rows-[1fr_1fr_200px] h-[calc(100%-48px)]">
+      <StatsBox title="This Year" count={3720} />
+      <MonthlyStatsBox  />
+      <Counter />
     </div>
   )
 }
---
+++ components/pages/project/Counter.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+
+export const Counter = () => {
+  const supabase = createClientComponentClient()
+  const [count, setCount] = useState<number>(20)
+  const submitRecord = async () => {
+    const { data, error } = await supabase
+    .from('records')
+    .insert([
+      {
+        project_id: 1, 
+        count: count,
+      },
+    ])
+    .select()
+  }
+
+  return (
+    <div className="mt-4">
+      <div className="flex flex-row gap-2 justify-between mb-4 h-20 items-center">
+        <div className="flex gap-2">
+          <Button onClick={() => setCount((prev) => prev-1)}>-1</Button>
+          <Button onClick={() => setCount((prev) => prev-5)}>-5</Button>
+        </div>
+        <h1 className="font-bold text-5xl">{count}</h1>
+        <div  className="flex gap-2">
+          <Button onClick={() => setCount((prev) => prev+5)}>+5</Button>
+          <Button onClick={() => setCount((prev) => prev+1)}>+1</Button>
+        </div>
+      </div>
+      <Button className="w-full mb-4" onClick={()=> submitRecord()}>Submit</Button>
+    </div>
+  )
+}
